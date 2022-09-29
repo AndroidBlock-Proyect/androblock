69e1131ff91b88c591aa7ddcd4c244370a6a8eb5
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-09-29 13:45:58 -0400

    css and renaming

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home'
+import NewDevice from './Pages/Newdevice'
+import DeviceList from './Pages/Devicelist'
+import Tools from './Pages/tools'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="NewDevice" element={<NewDevice/>} />
+          <Route path="DeviceList" element={<DeviceList/>} />
+          <Route path="Tools" element={<Tools/>} />
+      </Routes>
     </div>
   );
 }
