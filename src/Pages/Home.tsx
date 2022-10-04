ff0405ad5eee4f7dcf430c72e38a2ae652ab8d60
Luis071 <82769401+Luis071@users.noreply.github.com>
2022-10-04 15:46:49 -0400

    cambio

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,29 +4,55 @@ import "./css/home.css"
 
 const Home = () => {
     return(
-    
-       <body>
-        <header>
-            <nav>
-                <a href="/Devicelist">Lista de Disposivos</a>
-                <a href="/tools">Herramientas</a>
-                <a href="/Newdevice">Nuevo Disposivo</a>
-            </nav>
-            <section className= "textosheader">
-                <div className="textosheader">
-                    <h1>
-                        AndroidBlock📵 - Pagina Principal
-                    </h1>
-                    <br/>
-                    <img src="https://www.comocrearunapaginaweb.com.mx/wp-content/uploads/2022/06/Banner-en-Android_10424.png" alt="" />
+        
+        <div>
+
+            <header>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>
+                <section className= "textosheader">
+                    <div className="textosheader">
+                        <h1>
+                            AndroidBlock📵
+                        </h1>
+                        <br/>
+                        <img src="https://www.comocrearunapaginaweb.com.mx/wp-content/uploads/2022/06/Banner-en-Android_10424.png" alt="" />
+                    </div>
+                </section>
+            </header>
+
+            <body>
+                
+                <div className="global-container">
+                    <div className="card login-form">
+                        <div className="card-body">
+                            <h1 className="card-title text-center">Acceso AB</h1>
+                        </div>
+                        <div className="card-text">
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputUser1">Usuario</label>
+                                    <input type="user" className="form-control form-control-sm" id="exampleInputUser1" />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Contraseña</label>
+                                    <input type="password" className="form-control form-control-sm" id="exampleInputPassword1" />
+                                </div>
+
+                                <button type="submit" className="btn btn-primary btn-block">
+                                    Acceder
+                                </button>
+                                
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </section>
-        </header>
+
+            </body>
 
 
-
-       </body>
+        </div>
     )
 }
-
 export default Home;
