100ec342ac367331e3ecf69001d4e9c5270cd6c6
Luis071 <82769401+Luis071@users.noreply.github.com>
2022-10-07 11:45:49 -0400

    cambio

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,13 +10,21 @@ const Home = () => {
             <header>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>
+            <nav>
+                <a href="/Devicelist">Dispositivos</a>
+                <a href="/tools">Herramientas</a>
+                <a href="/Newdevice">Nuevo Disposivo</a>
+            </nav>
+            <br />
                 <section className= "textosheader">
                     <div className="textosheader">
+                    <img src="https://www.comocrearunapaginaweb.com.mx/wp-content/uploads/2022/06/Banner-en-Android_10424.png" alt="" />
+                    <br/>
+                    <br/>
                         <h1>
                             AndroidBlock📵
                         </h1>
                         <br/>
-                        <img src="https://www.comocrearunapaginaweb.com.mx/wp-content/uploads/2022/06/Banner-en-Android_10424.png" alt="" />
                     </div>
                 </section>
             </header>
